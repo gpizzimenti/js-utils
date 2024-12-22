06e0a58fc1e1cf08a7a3fe258f2b14d147b38d80
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2024-12-22 13:01:46 +0100

    + deserializeQueryStringToFormData,   escapeHtmlAttributeValue

--- formUtils.js
+++ formUtils.js
@@ -24,3 +24,28 @@ export const serializeFormToJSON = (form) => {
 };
 
 /*-----------------------------------------------------------------------------------------------*/
+
+export const deserializeQueryStringToFormData = (qs) => {
+	const formData = new FormData();
+
+	const params = new URLSearchParams(qs || window.location.search);
+
+	for (const [key, value] of params) {
+		formData.append(key, value);
+	}
+
+	return formData;
+};
+
+/*-----------------------------------------------------------------------------------------------*/
+
+export const escapeHtmlAttributeValue = (value) => {
+	return value
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#39;");
+};
+
+/*---------------------------------------------------------------------------------------*/
